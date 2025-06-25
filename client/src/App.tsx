36dbf6d9021c354fc60bdf21337966e3bfbc6eb2
sachin kumar <dev.sachin207@gmail.com>
2025-06-26 00:55:43 +0530

    bug fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,20 +6,33 @@ import DotGrid from "./components/parentBackgroud/DotGrid/DotGrid";
 function App() {
   return (
     <>
-      <div style={{ width: "100%", height: "100vh", position: "relative" }}>
-        <DotGrid
-          dotSize={2}
-          gap={15}
-          baseColor="#fff"
-          activeColor="#000"
-          proximity={120}
-          shockRadius={250}
-          shockStrength={5}
-          resistance={750}
-          returnDuration={1.5}
-        />
+      <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: 1,
+          }}
+        >
+          <DotGrid
+            dotSize={2}
+            gap={15}
+            baseColor="#fff"
+            activeColor="#000"
+            proximity={120}
+            shockRadius={250}
+            shockStrength={5}
+            resistance={750}
+            returnDuration={1.5}
+          />
+        </div>
+        <div style={{ position: "relative", zIndex: 2 }}>
+          <RouterProvider router={router} />
+        </div>
       </div>
-      <RouterProvider router={router} />
     </>
   );
 }
--- client/src/features/auth/pages/Login.tsx
+++ client/src/features/auth/pages/Login.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import AuthForm from "../components/auth-form";
-import DotGrid from "@/components/parentBackgroud/DotGrid/DotGrid";
 const Login: React.FC = () => {
   return (
     <>
